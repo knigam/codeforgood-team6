894b103d789c082f3a34cba63877794a9fe9b6b4
kushal <kushal.nigam1@gmail.com>
2014-10-11 08:00:01 -0400

    added mandrill support

--- gardN-rails/app/models/user.rb
+++ gardN-rails/app/models/user.rb
@@ -1,3 +1,4 @@
+require 'mandrill'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -5,4 +6,23 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   has_many :ratings
+ 
+  private
+  def send_email(text, email)
+        m = Mandrill::API.new '1bIbtiJDlLgJjqw4YuPegw'
+        message = {   
+         :subject=> "Someone messaged you on gardN!",  
+         :from_name=> "Room Mate",  
+         :text=> text,  
+         :to=>[  
+           {   
+             :email=> email
+           }   
+         ],  
+         :html=>"<html> #{text} </html>",  
+         :from_email=>"message@gardn.room-mate.me"  
+        }   
+        sending = m.messages.send message  
+  end 
+
 end
