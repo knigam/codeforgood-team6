c691ccbc0d41d7ec9e6aa81fd1563369f8d8dbd9
Yasasvi Tadavarthi <Yasasvi@Yasasvis-MacBook-Pro-2.local>
2014-10-11 03:38:52 -0400

    http get

--- gardN-android/app/src/main/java/gardn/codeforgood/com/gardn_android/activity/CommunityGardenActivity.java
+++ gardN-android/app/src/main/java/gardn/codeforgood/com/gardn_android/activity/CommunityGardenActivity.java
@@ -1,19 +1,71 @@
 package gardn.codeforgood.com.gardn_android.activity;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import gardn.codeforgood.com.gardn_android.R;
+import gardn.codeforgood.com.gardn_android.helper.HttpHelper;
 
 public class CommunityGardenActivity extends Activity {
-
+    private ListView resultListView;
+    private JSONObject array;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_garden);
-    }
 
+        new AsyncTask<Void, Void, Boolean>(){
+
+            @Override
+            protected Boolean doInBackground(Void... voids) {
+                String posts = null;
+
+                posts = getString(R.string.posts_index);
+                try{
+                    array = HttpHelper.httpGet(posts);
+                }catch(Exception e){
+                    System.out.print(e.getMessage());
+                    return false;
+                }
+                return true;
+
+            }
+
+            @Override
+            protected void onPostExecute(final Boolean success){
+                List<String> posts = new ArrayList<String>();
+//                for(int i = 0; i < array.length(); i++){
+//                    try {
+//                        JSONObject json = array.getJSONObject(i);
+//                        posts.add(json.getString("common_name"));
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+
+                if(success){
+                    ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(CommunityGardenActivity.this,
+                            android.R.layout.simple_list_item_1 );
+                    dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    resultListView.setAdapter(dataAdapter);
+                }
+                else {
+
+                }
+            }
+        }.execute(null, null, null);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
